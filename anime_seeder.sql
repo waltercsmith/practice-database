60adaca4e09207113b9afc2e33645519fb767fe1
Walter Smith <walter.c.smith01@gmail.com>
2021-03-18 10:42:19 -0400

    Used the INSERT command to add records to the table called anime

---
+++ anime_seeder.sql
@@ -0,0 +1,6 @@
+USE anime_db;
+
+insert into anime (debut_year, title, genre)
+values ('1997', 'Berserk', 'Action/Dark'),
+       ('2019', 'Kengan Ashura', 'Action/Martial Arts '),
+       ('2016', 'My Hero Academia', 'Action/Superhero');
