fe44bc30db23cb5556da364d090ce3e95371bdde
Walter Smith <walter.c.smith01@gmail.com>
2021-03-18 11:04:02 -0400

    Used SELECT statements to view data accordingly

--- anime_seeder.sql
+++ anime_seeder.sql
@@ -4,3 +4,22 @@ insert into anime (debut_year, title, genre)
 values ('1997', 'Berserk', 'Action/Dark'),
        ('2019', 'Kengan Ashura', 'Action/Martial Arts '),
        ('2016', 'My Hero Academia', 'Action/Superhero');
+
+# Going to view just the debut year and title
+
+select debut_year, title from anime;
+
+# To view all available columns from the table called anime
+
+select * from anime;
+
+# To view all data in the table called anime with the value of Action/Dark
+
+select * from anime where genre = 'Action/Dark';
+
+# To view all data in the table called anime with an id of 2
+
+select * from anime where id = 2;
+
+# Testing how to output arbitrary data
+
